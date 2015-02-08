6557823c96e89f51fb5cc7894545c2e6eaf5efeb
Guillermo Castillo <phasnox@gmail.com>
2015-02-08 13:40:58 -0500

    EKG setup update

--- scripts/ekg_setup.sh
+++ scripts/ekg_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Install dependencies for scipy
+sudo apt-get install gfortran libopenblas-dev liblapack-dev
+
 # Python dependencies
 sudo pip install tornado
 sudo pip install numpy
@@ -7,6 +10,7 @@ sudo pip install scipy
 
 # Dependencies for the webapp
 cd examples/ekg/frontend
-npm install -g bower
-npm install -g gulp
+sudo npm install -g bower
+sudo npm install -g gulp
 bower install
+npm install gulp
